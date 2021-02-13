a50ed887553d8ba7439ae5e0f182a70fdbbc3b85
Shingo Tanikawa <dubai2k@gmail.com>
2021-02-13 15:06:07 +0900

    replaceState

--- md-survey/src/shared/router.ts
+++ md-survey/src/shared/router.ts
@@ -1,4 +1,5 @@
 import { createRouter } from 'stencil-router-v2';
+import { getJson, setJson } from './store';
 
 export const Router = createRouter();
 
@@ -7,5 +8,10 @@ const href = document.head.querySelector('base').href;
 export const base = href.substr(origin.length, href.length - origin.length - 1);
 
 export function push(href: string) {
+  history.replaceState(getJson(), null);
   Router.push(base + href);
 }
+
+window.onpopstate = (e: PopStateEvent) => {
+  setJson(e.state);
+};
